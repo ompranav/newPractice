8fb74e85683acfa3d3d09908a67c6ab8b20e23ec
om <ompranav1993@gmail.com>
2021-02-23 10:50:20 +0530

    committed and pushed new program

--- MyProject/src/com/amazon/alexa/demo/DemoGit.java
+++ MyProject/src/com/amazon/alexa/demo/DemoGit.java
@@ -3,6 +3,8 @@ package com.amazon.alexa.demo;
 public class DemoGit {
 	public static void main(String[] args) {
 		System.out.println("checking github commit ");
+		System.out.println("ne line ");
+		
 	}
 
 }
